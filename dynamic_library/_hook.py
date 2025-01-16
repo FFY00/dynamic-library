a84d1e0de717a07ab022016416f0c0ae0790af07
Filipe Laíns <lains@riseup.net>
2025-01-16 07:32:37 +0000

    dl: fix Python <=3.11 on Windows
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- dynamic_library/_hook.py
+++ dynamic_library/_hook.py
@@ -15,4 +15,5 @@ if os.environ.get('PYTHON_DYNAMIC_LIBRARY_DISABLE'):
     warnings.warn('Libraries registered via dynamic_library entrypoints will not be loaded!', RuntimeWarning)
 else:
     for lib in dynamic_library.get_libraries():
+        lib = os.fspath(lib)  # Needed by Python <=3.11 on Windows
         ctypes.CDLL(lib, ctypes.RTLD_LOCAL)
