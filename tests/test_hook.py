8fa64a906650f44263f8062b9d7f0a13559656e1
Filipe Laíns <lains@riseup.net>
2025-01-16 06:59:22 +0000

    tests: fix binary patching on macOS
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- tests/test_hook.py
+++ tests/test_hook.py
@@ -7,6 +7,8 @@ import pytest
 
 import environment_helpers.build
 
+import dynamic_library
+
 
 def test_extension_with_dependency(env, tmp_path, packages, monkeypatch):
     env.install(['pkgconf'])
@@ -20,10 +22,12 @@ def test_extension_with_dependency(env, tmp_path, packages, monkeypatch):
     env.install_wheel(wheel)
 
     # Remove rpath, as meson insists on setting it
+    libfoo_path = os.path.join(env.scheme['platlib'], 'register_library', 'libfoo' + dynamic_library._EXT)
     uses_library_path = os.path.join(env.scheme['platlib'], 'uses_library' + sysconfig.get_config_var('EXT_SUFFIX'))
     if sys.platform == 'linux':
         subprocess.check_call(['patchelf', '--remove-rpath', uses_library_path])
     elif sys.platform == 'darwin':
+        subprocess.check_call(['install_name_tool', '-id', 'libfoo.dylib', libfoo_path])
         subprocess.check_call(['install_name_tool', '-change', '@rpath/libfoo.dylib', 'libfoo.dylib', uses_library_path])
 
     # Make sure uses_library.foo() works
